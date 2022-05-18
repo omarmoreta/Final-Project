046c41fa80b5405f84be279440a6f024189141e5
German <german.a.almonte@gmail.com>
2022-05-18 18:22:44 -0400

    added player, and player controls

--- src/assets/game.js
+++ src/assets/game.js
@@ -1,26 +1,33 @@
 import Phaser from "phaser";
+import Player from "./player.js";
 
 export default class MyGame extends Phaser.Scene {
-  constructor() {
-    super("thisGame");
-  }
+    constructor() {
+        super("thisGame");
+    }
 
-  preload() {
-    this.cameras;
-  }
+    preload() {
+        this.cameras;
+        this.player;
+        this.keys;
+    }
 
-  create() {
-    // FADE IN
-    this.cameras.main.fadeIn(1000, 0, 0, 0)
+    create() {
+        // FADE IN
+        this.cameras.main.fadeIn(1000, 0, 0, 0)
 
-    // CAMERA
-    this.physics.world.bounds.width = map.widthInPixels;
-    this.physics.world.bounds.height = map.heightInPixels;
-    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-    this.cameras.main.setRoundPixels(true);
-  }
+        // CAMERA
+        this.physics.world.bounds.width = map.widthInPixels;
+        this.physics.world.bounds.height = map.heightInPixels;
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.cameras.main.setRoundPixels(true);
+        // PLAYER
+        this.player = new Player(this, 225, 343, "knight").setScale(1);
+        this.player.body.setCollideWorldBounds(true);
+        this.cameras.main.startFollow(this.player, true, 0.1, 0.1);
+    }
 
-  update(time, delta) {
-    this.player.update();
-  }
+    update(time, delta) {
+        this.player.update();
+    }
 }
