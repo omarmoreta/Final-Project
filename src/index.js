1dab3b727b6f38c862f0ce1922839cbd29b95d2c
German <german.a.almonte@gmail.com>
2022-05-18 19:50:08 -0400

    added pause scene and pause ui

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { config } from "./config.js";
 import MyGame from "./assets/game";
 import BootScene from "./assets/bootscene";
 import MainmenuScene from "./assets/mainmenu";
+import PauseUI from "./assets/pauseui";
 
 class Game extends Phaser.Game {
   constructor() {
@@ -10,6 +11,7 @@ class Game extends Phaser.Game {
     this.scene.add("thisGame", MyGame);
     this.scene.add("Boot", BootScene);
     this.scene.add("Mainmenu", MainmenuScene);
+    this.scene.add("pause", PauseUI);
     this.scene.start("Boot");
   }
 }
